8a818637a09a33c25a5a0463ef927fe6beaf1659
Sahil Chouhan <chouhangolu604@gmail.com>
2025-11-25 11:38:45 +0530

    added new file

---
+++ 60.js
@@ -0,0 +1,10 @@
+//sort method -  is the method that change original array
+
+const numbers = [5,9, 1200, 400, 3000]
+
+const sortedNumbers = numbers.sort((a, b)=>a-b)
+console.log(sortedNumbers)
+
+const userNames = ['harshit', 'akash', 'vinay', 'bihari', 'Aakash']
+userNames.sort()
+console.log(userNames)
